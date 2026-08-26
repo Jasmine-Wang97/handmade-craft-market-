4cb9b4c6580a1bc13bfd2559fd447b1e715f5c0a
JessmieW <4a470112s@gmail.com>
2026-08-26 19:37:04 +1000

    SCRUM-19: Build product list API

---
+++ frontend/src/models/product.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Handmade Wool Art",
+    price: 45,
+    image: "/images/sample1.jpg",
+    description: "Beautiful handmade wool artwork."
+  },
+  {
+    id: 2,
+    name: "Vintage Fabric Patchwork",
+    price: 60,
+    image: "/images/sample2.jpg",
+    description: "Colorful patchwork made from vintage fabrics."
+  },
+  {
+    id: 3,
+    name: "Custom Embroidery Frame",
+    price: 35,
+    image: "/images/sample3.jpg",
+    description: "Personalized embroidery framed with love."
+  }
+];
+
+export default products;
